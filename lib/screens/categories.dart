ac36c816e891e18e2f33bb27671c10de32831c2a
z-rayc <zilerayser@gmail.com>
2023-10-19 11:15:53 +0200

    Optimize sliding animation

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -81,9 +81,15 @@ class _CategoriesScreenState extends State<CategoriesScreen>
             )
         ],
       ),
-      builder: (context, child) => Padding(
-          padding: EdgeInsets.only(top: 100 - _animationController.value * 100),
-          child: child),
+      builder: (context, child) => SlideTransition(
+        position: _animationController.drive(
+          Tween(
+            begin: const Offset(0, 0.2),
+            end: const Offset(0, 0),
+          ),
+        ),
+        child: child,
+      ),
     );
   }
 }
